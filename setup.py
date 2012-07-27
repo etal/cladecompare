881b28cabdbe7dab25dc343e93a9890cf37be890
Eric Talevich <eric.talevich@gmail.com>
2012-07-27 11:59:21 -0400

    setup.py: specify dependencies for setuptools/easy_install

--- setup.py
+++ setup.py
@@ -2,18 +2,31 @@
 
 """Tests for contrasting conservation between gene clades."""
 
+from os.path import dirname
+
+setup_args = {}
+
 try:
     from setuptools import setup
+    # Dependencies for easy_install:
+    setup_args.update(
+        install_requires=[
+            'Biopython >= 1.59',
+            'scipy >= 0.6', 
+        ])
 except:
     from distutils.core import setup
 
-setup(  name='CladeCompare',
-        version='dev',
-        description=__doc__,
-        author='Eric Talevich',
-        author_email='eric.talevich@gmail.com',
-        url='http://github.com/etal/cladecompare',
-        packages=['cladecompare'],
-        scripts=['cladecompare.py'],
-        )
+setup_args.update(
+    name='CladeCompare',
+    version='dev',
+    description=__doc__,
+    author='Eric Talevich',
+    author_email='etal@uga.edu',
+    url='http://github.com/etal/cladecompare',
+    packages=['cladecompare'],
+    scripts=[(dirname(__file__) or '.') + '/cladecompare.py'],
+)
+
+setup(**setup_args)
 

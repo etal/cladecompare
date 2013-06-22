466c50fb50f35f299d3035dbd81aeaee3db518a7
Eric Talevich <eric.talevich@gmail.com>
2013-06-22 12:14:06 -0400

    Set version to 0.2-dev

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ DIR = (dirname(__file__) or '.') + '/'
 
 setup_args.update(
     name='CladeCompare',
-    version='0.1.0',
+    version='0.2-dev',
     description=__doc__,
     author='Eric Talevich',
     author_email='etal@uga.edu',
